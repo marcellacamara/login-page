ea2182ff9c62df8989380cc6bdb05456e5f3093b
marcella <marcella.camara@sagatechbrasil.com.br>
2022-12-23 10:14:07 -0300

    estilização feita

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  width: 100%;
+  height: 42px;
+  background-color: #bc9ac5;
+  color: #fff;
+
+  border: 1px solid #bc9ac5;
+  border-radius: 21px;
+
+  &:hover {
+    opacity: 0.6;
+    cursor: pointer;
+  }
+`;
